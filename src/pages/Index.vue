396b192255dc029baf0b05185c68a3e6cb8c5260
Pascal Chorus <p.chorus@me.com>
2021-09-23 20:50:03 +0200

    feat(homepage): add section Yogahäuschen

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,16 @@
 <template>
   <Layout>
     <LandingPageHero id="willkommen"></LandingPageHero>
+    <YogahaeuschenSection id="mirka-und-das-yogahaeuschen"></YogahaeuschenSection>
   </Layout>
 </template>
 
 <script>
 import LandingPageHero from '../components/LandingPageHero';
+import YogahaeuschenSection from '../components/YogahaeuschenSection';
 
 export default {
-  components: { LandingPageHero },
+  components: { LandingPageHero, YogahaeuschenSection },
   metaInfo: {
     title: 'Mirkas Yogahäuschen',
   },

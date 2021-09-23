0af1a204873661fd8b0b7675b093e5cbd410d250
Pascal Chorus <p.chorus@me.com>
2021-09-23 21:44:06 +0200

    feat(homepage): add section Yoga & mehr

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,17 +2,41 @@
   <Layout>
     <LandingPageHero id="willkommen"></LandingPageHero>
     <YogahaeuschenSection id="mirka-und-das-yogahaeuschen"></YogahaeuschenSection>
+    <YogaUndMehrSection id="yoga-und-mehr"></YogaUndMehrSection>
+    <div class="illustration">
+      <g-image
+        class="illustration__image"
+        src="@/assets/images/yoga-mirka.png"
+        width="1000"
+        :immediate="true"
+      ></g-image>
+    </div>
   </Layout>
 </template>
 
 <script>
 import LandingPageHero from '../components/LandingPageHero';
 import YogahaeuschenSection from '../components/YogahaeuschenSection';
+import YogaUndMehrSection from '../components/YogaUndMehrSection';
 
 export default {
-  components: { LandingPageHero, YogahaeuschenSection },
+  components: { LandingPageHero, YogahaeuschenSection, YogaUndMehrSection },
   metaInfo: {
     title: 'Mirkas Yogahäuschen',
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import '../assets/variables';
+
+.illustration {
+  padding: $space-xl;
+  text-align: center;
+
+  &__image {
+    width: 100%;
+    max-width: 500px;
+  }
+}
+</style>

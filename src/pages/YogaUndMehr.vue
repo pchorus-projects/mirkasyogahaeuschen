e8692602ef82ece29697d8768b95c468135f03b2
Pascal Chorus <p.chorus@me.com>
2021-10-31 16:19:52 +0100

    feat(yoga-und-mehr): add yoga-und-mehr page

---
+++ src/pages/YogaUndMehr.vue
@@ -0,0 +1,121 @@
+<template>
+  <Layout>
+    <YogaUndMehrHero></YogaUndMehrHero>
+    <h1 class="u-heading">Yoga & mehr</h1>
+
+    <ul>
+      <li :id="item.to" class="yoga-list__item u-margin-bottom-xxl" v-for="item in yogaUndMehrItems" :key="item.to">
+        <h2 class="yoga-list__title">{{ item.name }}</h2>
+        <g-image
+          class="yoga-list__image"
+          :src="require(`!!assets-loader?width=1536!@assets/images/yoga-und-mehr-thumbnails/${item.image}`)"
+        />
+        <p class="yoga-list__text">{{ item.text }}</p>
+      </li>
+    </ul>
+  </Layout>
+</template>
+
+<script>
+import YogaUndMehrHero from '../components/YogaUndMehrHero';
+
+export default {
+  components: { YogaUndMehrHero },
+  metaInfo: {
+    title: 'Yoga & mehr',
+  },
+  data: function () {
+    return {
+      yogaUndMehrItems: [
+        {
+          name: 'Familien Yoga',
+          to: 'familien-yoga',
+          image: 'familien-yoga.jpg',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        },
+        {
+          name: 'Personal Training',
+          to: 'personal-training',
+          image: 'personal-training.jpg',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        },
+        {
+          name: 'Kinder Yoga',
+          to: 'kinder-yoga',
+          image: 'kinder-yoga.jpg',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        },
+        {
+          name: 'Ätherische Öle',
+          to: 'aetherische-oele',
+          image: 'aetherische-oele.jpg',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        },
+        {
+          name: 'Yin Yoga',
+          to: 'yin-yoga',
+          image: 'yin-yoga.jpg',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        },
+        {
+          name: 'Online Yoga',
+          to: 'online-yoga',
+          image: 'online-yoga.jpg',
+          text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/variables';
+
+.yoga-list {
+  &__item {
+    width: 768px;
+    margin: 0 auto $space-xxl auto;
+  }
+
+  &__title {
+    text-transform: uppercase;
+    margin-bottom: $space-m;
+    text-align: center;
+    padding: 0 $space-m;
+  }
+
+  &__image {
+    width: 100%;
+    height: 480px;
+    object-fit: cover;
+    object-position: top;
+    margin-bottom: $space-m;
+    text-align: center;
+  }
+}
+
+@media (max-width: $max-width-mobile-landscape) {
+  .yoga-list {
+    &__item {
+      width: 100%;
+      margin: 0 auto $space-xxl auto;
+    }
+
+    &__image {
+      height: 280px;
+    }
+
+    &__text {
+      padding: 0 $space-m;
+    }
+  }
+  //  .yoga-list {
+  //    &__item {
+  //      width: 100%;
+  //      height: 300px;
+  //      margin: 0 auto;
+  //    }
+  //  }
+}
+</style>

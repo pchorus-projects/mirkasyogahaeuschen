a0df8e8e5e8b9189260613466c3a7b36ca4e29f8
Pascal Chorus <p.chorus@me.com>
2021-11-03 21:35:32 +0100

    feat(homepage): add texts for Yogahäuschen

--- src/components/YogahaeuschenSection.vue
+++ src/components/YogahaeuschenSection.vue
@@ -8,19 +8,58 @@
       "When you do things from your soul, you feel a river moving in you. Listen to your soul, trust your heart, feel,
       believe, ..."
     </p>
-    <p class="text">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-      dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
-      kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-      sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-      sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
-      duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis
-      autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat
-      nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue
-      duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
-    </p>
+    <div class="text">
+      <p>
+        Herzlich Willkommen auf der Seite zu meinem Yogahäuschen. Ich bin Mirka, Aachenerin und habe Yoga sehr ins Herz
+        geschlossen, weil ich über die Praxis Jahr für Jahr mehr und mehr zu mir gereist bin. Mit mehr Bewusstsein,
+        Achtsamkeit, Zufriedenheit und Dankbarkeit gehe ich dank Yoga durchs Leben. Das Schöne ist, dass dieser Weg
+        niemals endet. Meine Lehrerin hat mir dazu ein passendes Mantra mitgegeben: „Been travelling a day, been
+        travelling a year, been travelling a lifetime to find my way home. Home is where my heart is, my heart is my
+        home.“
+      </p>
+      <p>
+        Auf meinem Yogaweg habe ich nicht nur mich selbst besser kennengelernt, sondern viele besondere und spannende
+        Menschen und weitere wertvolle Unterstützer, wie die Ätherischen Öle, Musik, Coaching-Wege, Berührung.
+      </p>
+      <p>
+        So gerne teile ich mit anderen Menschen über die Energiearbeit meine persönliche und fachliche Erfahrung,
+        erinnere ans Achtsam sein, unterstütze dabei, über Yoga mehr Raum zum Fühlen zu öffnen und den Weg in ein Mehr
+        an Selbstliebe zu wählen.
+      </p>
+      <p>
+        Im Herbst 2021 hat mich ein persönliches Ereignis in diesem kleinen Reihenhäuschen in Aachen-Richterich stehen
+        lassen. Trotz schwieriger Zeiten entschied ich mich dafür, auf Bauch und Herz zu hören und hier Raum für
+        Erholung, Entspannung und Begegnung zu schaffen.
+      </p>
+      <p>
+        Das kleine Haus wurde von seinen Eigentümern 9 Monate lang kernsaniert und vielleicht sollte es so sein, dass
+        erst einmal Ruhe einkehrt und über den langen Lockdown ein Winter-Savasana an der Reihe sein sollte, damit
+        anschließend die Energie stimmt und mein Wunsch, dass ihr hier Ruhe und Frieden findet, aufgeht.
+      </p>
+      <p>
+        Seit diesem Sommer ist die Tür geöffnet und ich freue mich sehr, dass meine Vision anfangen darf, zu leben. Ihr
+        sollt euch wie zuhause fühlen. Zeit für euch finden. Das Yogahäuschen soll für euch ein Ort zur Selbsterfahrung,
+        Transformation und Regeneration sein.
+      </p>
+      <p>
+        Die Angebote für euch wachsen, ihr seid herzlich Willkommen zum Tee trinken, im Garten sein, Entspannen,
+        Meditieren, Yoga praktizieren, Auszeiten genießen.
+      </p>
+      <p>
+        Mehr zu mir:<br />
+        Seit 2007 auf dem Yogaweg<br />
+        Yin Yogalehrerin und - Coach I verliebt-in-yoga, Andrea Huson, Köln<br />
+        Yogalehrerin (300 Std.) I Om Shanti, Ratingen<br />
+        Vanessa Stilp & Silja Mahlow (Yoga & Yogatherapie, Coaching)<br />
+        Beate Meyer (Anatomie)<br />
+        Dr. Maitreya Larios (Philosophie)<br />
+        Polarity Therapie I Emmanuelle Collinet<br />
+        Aroma Yogalehrerin I Emmanuelle Collinet<br />
+        Kinderyogalehrerin I Yoga & Familie, Carolin Richard, Münster<br />
+        Teilnahme an diversen Yoga-Workshops zu verschiedenen Themen, wie Yin Yoga, Yoga Nidra, Women circles, Tantra,
+        Detox, Arm Balances, Thai Yoga Massage
+      </p>
+    </div>
   </section>
 </template>
 
@@ -47,5 +86,9 @@ export default {};
 
 .text {
   padding: 0 $space-xl $space-xl $space-xl;
+
+  > p {
+    padding-bottom: $space-m;
+  }
 }
 </style>

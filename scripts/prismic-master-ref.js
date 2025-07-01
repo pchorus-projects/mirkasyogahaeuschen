cdde61b8112902520ea56278b6e04aa77aced423
Pascal Chorus <p.chorus@me.com>
2025-07-01 20:06:47 +0200

    build(nuxt): initial commit using nuxt

--- scripts/prismic-master-ref.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-const dotenv = require('dotenv').config();
-
-async function getMasterRef(url, prismicAccessToken) {
-  const res = await axios.get(`${url}/api?access_token=${prismicAccessToken}`, { method: 'GET' });
-  return res.data.refs.find((ref) => ref.id === 'master').ref;
-}
-
-getMasterRef('https://mirkasyogahaeuschen.prismic.io', process.env.PRISMIC_TOKEN).then(console.log);
--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const coursePlanUrl = 'https://www.eversports.de/s/mirkas-yogahauschen';
-export const mailtoMirkaHref = 'mailto:mirka@mirkasyogahaeuschen.de';
